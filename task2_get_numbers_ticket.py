065d35e319fb89916fb890fa1ac55cb4322d390c
Oleksandr Berezovskyi <berezovskiy.a@gmail.com>
2026-01-19 18:02:28 +0200

    refactor for Task 2: type hints added, docstrings, and fixed return values

--- task2_get_numbers_ticket.py
+++ task2_get_numbers_ticket.py
@@ -1,33 +1,25 @@
 import random
 
-
-def get_numbers_ticket(min_val, max_val, quantity):
-
-    # Генерує набір унікальних та відсортованих випадкових чисел для лотереї.
-    # min_val (int): Мінімальне число (>= 1).
-    # max_val (int): Максимальне число (<= 1000).
-    # quantity (int): Кількість чисел для вибору.
-    # Returns:
-    # list: Відсортований список унікальних чисел або пустий список при невалідних даних.
-    # 1. Перевірка валідності вхідних даних
-    # - min має бути >= 1
-    # - max має бути <= 1000
-    # - quantity має бути в межах від 1 до (max - min + 1)
+def get_numbers_ticket(min_val: int, max_val: int, quantity: int) -> list:
+    """
+    Generates a set of unique sorted random numbers for a lottery.
+    Args:
+        min_val (int): Minimum possible number (>= 1).
+        max_val (int): Maximum possible number (<= 1000).
+        quantity (int): Number of digits to choose.
+    Returns:
+        list: Sorted list of unique numbers or an empty list if parameters are invalid.
+    """
 
     if not (1 <= min_val <= max_val <= 1000) or not (0 < quantity <= (max_val - min_val + 1)):
-        return "Ви ввели некоретні дані. Доступний діапазон від 1 до 1000."
+        return []
 
-    # 2. Генерація числен
-    # random.sample забезпечить унікальність кожного числа в наборі
     try:
         numbers = random.sample(range(min_val, max_val + 1), quantity)
-        # 3. Повернення відсортованого списку
         return sorted(numbers)
     except (ValueError, TypeError):
-        # Додаткова перевірка при некоректних даних на інпуті
         return []
 
 
-# Приклад використання:
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
